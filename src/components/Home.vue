e834e5a3869f206c4f514cc96019a473a50a44a1
bremp <byadao@gmail.com>
2020-03-10 16:57:53 -1000

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Goals</h1>
+    <ul>
+      <li v-for="goal in goals" :key="goal">{{ goal.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      goals: [
+        {
+          title: "Vuetify UI."
+        },
+        {
+          title: "Create Goal List Component"
+        },
+        {
+          title: "Create GoalAdd Component"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
