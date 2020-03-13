7eb61ab7c675ca14eb01a60c53ce4ebfc6765d9c
bremp <byadao@gmail.com>
2020-03-12 19:44:35 -1000

    Refactored GoalAdd component.

--- src/components/GoalList.vue
+++ src/components/GoalList.vue
@@ -3,10 +3,10 @@
     <v-row align="center" justify="center">
       <v-list rounded>
         <!-- <v-subheader>Goals</v-subheader> -->
-        <v-list-item-group v-model="item" color="primary">
-          <v-list-item v-for="(item, i) in goals" :key="i">
+        <v-list-item-group v-model="goal" color="primary">
+          <v-list-item v-for="(goal, i) in goals" :key="i">
             <v-list-item-content>
-              <v-list-item-title v-text="item.title"></v-list-item-title>
+              <v-list-item-title v-text="goal.title"></v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
@@ -18,40 +18,7 @@
 <script>
 export default {
   name: "GoalList",
-  props: {
-    source: String
-  },
   data: () => ({
-    items: [
-      { icon: "mdi-contacts", text: "Contacts" },
-      { icon: "mdi-history", text: "Frequently contacted" },
-      { icon: "mdi-content-copy", text: "Duplicates" },
-      {
-        icon: "mdi-chevron-up",
-        "icon-alt": "mdi-chevron-down",
-        text: "Labels",
-        model: true,
-        children: [{ icon: "mdi-plus", text: "Create label" }]
-      },
-      {
-        icon: "mdi-chevron-up",
-        "icon-alt": "mdi-chevron-down",
-        text: "More",
-        model: false,
-        children: [
-          { text: "Import" },
-          { text: "Export" },
-          { text: "Print" },
-          { text: "Undo changes" },
-          { text: "Other contacts" }
-        ]
-      },
-      { icon: "mdi-settings", text: "Settings" },
-      { icon: "mdi-message", text: "Send feedback" },
-      { icon: "mdi-help-circle", text: "Help" },
-      { icon: "mdi-cellphone-link", text: "App downloads" },
-      { icon: "mdi-keyboard", text: "Go to the old version" }
-    ],
     goals: [
       {
         title: "Vuetify UI."
