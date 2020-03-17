4873cc09990dcf6bd36d54db23b9bea1d570f937
bremp <byadao@gmail.com>
2020-03-16 18:46:17 -1000

    Added draggable list items.

--- src/components/GoalList.vue
+++ src/components/GoalList.vue
@@ -1,21 +1,26 @@
 <template>
   <v-row>
-    <v-list rounded width="100%" v-sortable>
-      <!-- <v-subheader>Goals</v-subheader> -->
+    <v-list rounded width="100%">
       <v-list-item-group v-model="goal" color="primary" align="start">
-        <v-list-item v-for="(goal, i) in goals" :key="i">
-          <v-list-item-content>
-            <v-list-item-title v-text="goal.title"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+        <draggable v-model="goals" @start="drag = true" @end="drag = false">
+          <v-list-item v-for="(goal, i) in goals" :key="i">
+            <v-list-item-content>
+              <v-list-item-title v-text="goal.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </draggable>
       </v-list-item-group>
     </v-list>
   </v-row>
 </template>
 
 <script>
+import draggable from "vuedraggable";
 export default {
   name: "GoalList",
-  props: ["goals"]
+  props: ["goals"],
+  components: {
+    draggable
+  }
 };
 </script>
